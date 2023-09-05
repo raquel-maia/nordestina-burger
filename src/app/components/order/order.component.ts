466eefc7cdcf6e64bf162cae5ae7f058bc1c630d
Raquel Maia <raquelmaia860@gmail.com>
2023-09-05 11:51:10 -0300

    enviar produtos

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductsService } from 'src/app/services/products/products.service';
+import { LocalStorageService } from 'src/app/services/localStorage/local-storage.service';
+import { OrderService } from 'src/app/services/orders/orders.service';
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
@@ -12,10 +14,15 @@ export class OrderComponent implements OnInit {
   type: string = 'hamburgueres';
   clientName: string = '';
   showErrorMessage: boolean = false;
+  garcon:any={};
+  errorMessage:string = '';
+
 
   constructor(
     private dataService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: LocalStorageService,
+    private orderService: OrderService,
   ) {}
 
   onClick(param: string) {
@@ -24,10 +31,23 @@ export class OrderComponent implements OnInit {
       this.items = data.filter((item: any) => item.type === this.type);
     });
   }
+
+  addToCart(item: any) {
+    console.log(item);
+    const cartItemIndex = this.destinoItems.findIndex(cartItem => cartItem.id === item.id);
+    console.log(cartItemIndex)
+    if (cartItemIndex === -1) {
+      const cartItem = { ...item, quantity: 1 };
+      this.destinoItems.push(cartItem);
+    } else {
+      this.destinoItems[cartItemIndex].quantity++;
+    }
+    console.log(this.destinoItems)
+  }
   
 
   moverItem(item: any) {
-    const itemCopy = { ...item, quantity: 1 };
+    const itemCopy = { ...item, quantity: 0 };
     this.destinoItems.push(itemCopy);
   }
 
@@ -39,6 +59,12 @@ export class OrderComponent implements OnInit {
     if (item.quantity > 0) {
       item.quantity--;
     }
+  }
+
+  formatarData() {
+    const dataAtual = new Date();
+    const dataFormatada = `${dataAtual.getFullYear()}-${(dataAtual.getMonth() + 1).toString().padStart(2, '0')}-${dataAtual.getDate().toString().padStart(2, '0')} ${dataAtual.getHours().toString().padStart(2, '0')}:${dataAtual.getMinutes().toString().padStart(2, '0')}:${dataAtual.getSeconds().toString().padStart(2, '0')}`;
+    return dataFormatada;
   }
 
   removeItem(product: any) {
@@ -54,10 +80,17 @@ export class OrderComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.buscarDadosGarcon()
     this.dataService.getItems().subscribe((data: any) => {
       this.items = data.filter((item: any) => item.type === this.type);
     });
     
+  }
+
+
+  buscarDadosGarcon(){
+    this.garcon= this.storage.getItem("user_data")
+
   }
 
   goToLogin(): void {
@@ -65,13 +98,50 @@ export class OrderComponent implements OnInit {
   }
 
   enviar() {
-    if (this.clientName.trim() === '') {
+
+/*     if (this.clientName.trim() === '') {
       this.showErrorMessage = true;
     } else {
       this.showErrorMessage = false;
-      // Atualizar depois para página de cozinha
-      this.router.navigate(['/cardapio']);
+
+    }  */
+
+    const pedido = {
+      userId:this.garcon.id,
+      clientName: this.clientName,
+      products: this.destinoItems.map(cartItem => ({
+        product: cartItem,
+        qty: cartItem.quantity,
+        dateEntry: this.formatarData()
+      })),
+      status: "pending",
+      dateEntry:this.formatarData()
+
+
     }
+    console.log(pedido)
+  
+
+    this.orderService.addOrder(pedido).subscribe(
+      response => {
+        this.errorMessage = '';
+        alert('Pedido enviado com sucesso!');
+        this.destinoItems = [];
+        this.clientName = "";
+        const currentTime = this.formatarData();
+        this.destinoItems.forEach(item => {
+          item.dateCompleted = currentTime;
+        });
+      },
+      error => {
+        console.error('Erro ao enviar pedido:', error);
+        this.errorMessage = 'Erro ao enviar o pedido. Por favor, tente novamente mais tarde.';
+      }
+    );
+  
+
   }
+
+  
 
 }
